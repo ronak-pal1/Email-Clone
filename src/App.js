5d9aabfb03057977e9d683053e988380386fb816
Ronak Paul <ronakpaul882@gmail.com>
2023-09-06 23:06:31 +0530

    Added new components and make routes int App component
    Signed-off-by: Ronak Paul <ronakpaul882@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Mail from "./Mail";
+import EmailList from "./EmailList";
+import {BrowserRouter as Router,  Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-        <Header />
-        <Sidebar />
-    </div>
+    <Router>
+      <div className="app">
+          <Header />
+
+          <div className="app__body">
+            <Sidebar />
+
+            <Routes>
+              <Route path="/mail" element={<Mail />} />
+
+              <Route path="/" element={<EmailList />} />
+            </Routes>
+          </div>
+      </div>
+    </Router>
   );
 }
 
